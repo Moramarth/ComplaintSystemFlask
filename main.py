9859e7adb5363f114d492847029874decd7a3e7b
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-04-13 07:44:56 +0300

    Initial db migration

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
 
 from db import db
+from resources.routes import routes
 
 app = Flask(__name__)
+app.config.from_object("config.DevelopmentConfig")
+
 db.init_app(app)
+api = Api(app)
+migrate = Migrate(app, db)
 
 
 @app.after_request
@@ -11,6 +18,8 @@ def close_request(response):
     db.session.commit()
     return response
 
+
+[api.add_resource(*route) for route in routes]
 
 if __name__ == '__main__':
     app.run()
--- managers/auth.py
+++ managers/auth.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 
 import jwt
 from decouple import config
+from flask_httpauth import HTTPTokenAuth
+from werkzeug.exceptions import Unauthorized
 
 
 class AuthManager:
@@ -9,3 +11,23 @@ class AuthManager:
     def encode_token(user):
         payload = {"sub": user.id, "exp": datetime.utcnow() + timedelta(days=2), "type": user.__class__.__name__}
         return jwt.encode(payload, key=config("JWT_SECRET"), algorithm="HS256")
+
+    @staticmethod
+    def decode_token(token):
+        try:
+            info = jwt.decode(jwt=token, key=config("JWT_SECRET"), algorithms=["HS256"])
+            return info["sub"], info["type"]
+        except Exception as ex:
+            raise ex
+
+
+auth = HTTPTokenAuth(scheme="Bearer")
+
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        user_id, type_user = AuthManager.decode_token(token)
+        return eval(f"{type_user}.query.filter_by(id={user_id}).first()")
+    except Exception:
+        raise Unauthorized("Invalid or missing token")
